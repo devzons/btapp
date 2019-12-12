3b890e7a1405a155b40835966b8f87b5a4918520
devzons <devzons@devzonss-iMac.local>
2019-12-12 12:46:00 -0800

    fetching data with Eloquent, add paginate() function on Posts

--- app/Post.php
+++ app/Post.php
@@ -6,5 +6,10 @@ use Illuminate\Database\Eloquent\Model;
 
 class Post extends Model
 {
-    //
+    // Table Name, can change the table name here
+    protected $table = 'posts';
+    //Primary Key
+    protected $primaryKey = 'id';
+    // Timestamps
+    public $timestamps = true;
 }
